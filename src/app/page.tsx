9420836603e5b5051975b7154512d6ce882f9efb
sweetArizCider <arizah.applecider+github@gmail.com>
2025-08-30 09:10:23 -0700

    config: :heavy_plus_sign: GSA-5-APPLECIDER | adds components libraries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,35 @@
 import Image from "next/image";
+import { ThemeInit } from "../../.flowbite-react/init";
 
-export default function Home() {
+
+export default function App() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol
-          className="
-          font-mono 
-          list-inside 
-          list-decimal 
-          text-sm/6 
-          text-center 
+    <>
+      <ThemeInit />
+      <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+        <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+          <Image
+            src="/next.svg"
+            alt="Next.js logo"
+            width={180}
+            height={38}
+            priority
+          />
+          <ol
+            className="
+          font-mono
+          list-inside
+          list-decimal
+          text-sm/6
+          text-center
           sm:text-left"
-        >
-          <li className="mb-2 tracking-[-.01em]">
-            Welcome to Glass Store TRC!
-          </li>
-        </ol>
-      </main>
-    </div>
+          >
+            <li className="mb-2 tracking-[-.01em]">
+              Welcome to Glass Store TRC!
+            </li>
+          </ol>
+        </main>
+      </div>
+    </>
   );
 }
