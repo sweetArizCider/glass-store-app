9ff2182713a4f01b7123c03bc9b6046ef9c6bfb2
sweetArizCider <arizah.applecider+github@gmail.com>
2025-09-01 02:48:35 -0700

    chore: :pencil2: GSA-6-APPLECIDER | makes copilot's recommendations

--- src/app/components/typography/TypographyComponent.tsx
+++ src/app/components/typography/TypographyComponent.tsx
@@ -1,6 +1,6 @@
 import type React from "react";
 
-type BodyType = "h1" | "h2" | "h3" | "body1" | "body2" | "";
+type BodyType = "h1" | "h2" | "h3" | "body1" | "body2";
 type ColorType = "blue" | "black" | "white" | "link";
 type AlignType = "left" | "right" | "end" | "start" | "center";
 interface TypographyProps {
@@ -22,24 +22,37 @@ export const TypographyComponent = (typographyProps: TypographyProps) => {
   } = typographyProps;
 
   const getColor = (color: ColorType) => {
-    switch (color) {
-      case "black":
-        return "text-black";
-      case "blue":
-        return "text-blue-1000";
-      case "link":
-        return "text-blue-450";
-      case "white":
-        return "text-white";
-    }
+    const colorClassMap = {
+      black: "text-black",
+      blue: "text-blue-1000",
+      link: "text-blue-450",
+      white: "text-white",
+    };
+    return colorClassMap[color];
   };
+
+  const getAlignClass = (align: AlignType) => {
+    const alignClassMap = {
+      left: "text-left",
+      right: "text-right",
+      center: "text-center",
+      start: "text-start",
+      end: "text-end",
+    };
+    return alignClassMap[align] || "text-left";
+  };
+
+  const alignClass = getAlignClass(align);
+  const isMontserrat = montserrat ? "font-montserrat" : null;
+  const isBold = bold ? "font-bold" : null;
+  const colorClass = getColor(color);
 
   const renderTypography = (bodyType: BodyType) => {
     switch (bodyType) {
       case "h1":
         return (
           <h1
-            className={`text-6xl font-montserrat ${isBold ?? "font-extrabold"} text-pretty text-${align}`}
+            className={`text-6xl font-montserrat ${isBold ?? "font-extrabold"} text-pretty ${alignClass}`}
           >
             {children}
           </h1>
@@ -47,7 +60,7 @@ export const TypographyComponent = (typographyProps: TypographyProps) => {
       case "h2":
         return (
           <h2
-            className={`text-4xl font-montserrat ${isBold ?? "font-medium"} ${typographyColor} text-pretty`}
+            className={`text-4xl font-montserrat ${isBold ?? "font-medium"} ${colorClass} text-pretty ${alignClass}`}
           >
             {children}
           </h2>
@@ -55,7 +68,7 @@ export const TypographyComponent = (typographyProps: TypographyProps) => {
       case "h3":
         return (
           <h3
-            className={`text-3xl font-montserrat ${isBold ?? "font-medium"} ${typographyColor} text-pretty`}
+            className={`text-3xl font-montserrat ${isBold ?? "font-medium"} ${colorClass} text-pretty ${alignClass}`}
           >
             {children}
           </h3>
@@ -63,7 +76,7 @@ export const TypographyComponent = (typographyProps: TypographyProps) => {
       case "body1":
         return (
           <p
-            className={`text-xl ${isMontserrat} ${isBold ?? "font-normal"} ${typographyColor} text-pretty`}
+            className={`text-xl ${isMontserrat} ${isBold ?? "font-normal"} ${colorClass} text-pretty ${alignClass}`}
           >
             {children}
           </p>
@@ -71,15 +84,12 @@ export const TypographyComponent = (typographyProps: TypographyProps) => {
       case "body2":
         return (
           <p
-            className={`text-xs ${isMontserrat} ${isBold ?? "font-normal"} ${typographyColor} text-pretty`}
+            className={`text-xs ${isMontserrat} ${isBold ?? "font-normal"} ${colorClass} text-pretty ${alignClass}`}
           >
             {children}
           </p>
         );
     }
   };
-  const isMontserrat = montserrat ? "font-montserrat" : null;
-  const isBold = bold ? "font-bold" : null;
-  const typographyColor = getColor(color);
   return renderTypography(body);
 };
